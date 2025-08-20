573a5dbe5589aa4e332527d7928553a593e7126c
thepaedu <patrick.bettler@icloud.com>
2025-08-20 20:27:12 +0200

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <script setup>
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-<p>Hallo Welt</p>
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
