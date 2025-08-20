a7cb6c676e67092612d5837a3c391e242ad9b1b3
thepaedu <patrick.bettler@icloud.com>
2025-08-20 20:41:39 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/patrick_bettler/', // <--- hier Repository-Namen eintragen
+  base: '/patrickbettler/', // <--- hier Repository-Namen eintragen
   plugins: [vue()],
 })
